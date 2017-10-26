42a0c7989c4f7068a8b79b69d70a4d4ab9473ae0
egin10 <wweusz@gmail.com>
2017-10-27 06:35:32 +0700

    edit view

--- theBox/app/urls.py
+++ theBox/app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^scan', views.scan, name="KicomAVscan"),
+    url(r'^ssh', views.ssh, name="ssh"),
+    url(r'^vtscan', views.vtscan, name="vtScan"),
 ]
--- theBox/app/views.py
+++ theBox/app/views.py
@@ -6,4 +6,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'index' : 'active'})
+
+def scan(request):
+    return render(request, 'scan.html', {'scan' : 'active'})
+
+def ssh(request):
+    return render(request, 'ssh.html', {'ssh' : 'active'})
+
+def vtscan(request):
+    return render(request, 'virustotal.html', {'vtscan' : 'active'})
