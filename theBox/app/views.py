e9c74423f17ae2381653e36cf860e245842eeec0
egin10 <wweusz@gmail.com>
2017-10-27 06:47:59 +0700

    edit url

--- theBox/app/views.py
+++ theBox/app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
--- theBox/theBox/urls.py
+++ theBox/theBox/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^monitor/', include('app.urls')),
+    # url(r'^monitor/', include('app.urls')),
+    url(r'^', include('app.urls')),
 ]
