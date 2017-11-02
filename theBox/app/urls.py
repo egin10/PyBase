40dd2f579f71df6362fe5b8a16777dad840c9248
egin10 <egin066@gmail.com>
2017-11-02 16:44:53 +0700

    edit

--- theBox/app/urls.py
+++ theBox/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^scan', views.scan, name="KicomAVscan"),
+    url(r'^scan', views.scan, name="scan"),
     url(r'^vtscan', views.vtscan, name="vtScan"),
     url(r'^command/(?P<path>\D+)', views.cmd),
 ]
